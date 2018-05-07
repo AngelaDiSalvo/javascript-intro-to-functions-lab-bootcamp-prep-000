10b66e01ffce3eff25f21d44b2e5edf87da5068f
Angela DiSalvo <angela.disalvo@gmail.com>
2018-05-07 12:15:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
-     return "YES INDEED!"
-
-   
-  } 
+    return "YES INDEED!"
+  } else if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  } else if 
 }
 //if (string.toLowerCase() === string) {
   //function sayHiToGrandma(string) {
