f8bc154d314992933791d40193e52227f3ef88ae
Angela DiSalvo <angela.disalvo@gmail.com>
2018-05-07 11:24:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var string = "hello!"
+var uppercase = "HELLO!"
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+lowercase.toLowerCase() === lowercase // true
+ 
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+if (string.toUpperCase() === uppercase) {
+  function sayHiToGrandma(string) {
+    return "YES INDEED!"
+  } 
+} else if (string.toLowerCase() === lowercase) {
+  function sayHiToGrandma(string) {
+    return "I can't hear you!"
+  }
+} 
